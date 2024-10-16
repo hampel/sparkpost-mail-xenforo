7e1953419202fd51e9306772be5392adec58371b
Simon Hampel <simon@hampelgroup.com>
2024-10-16 11:37:03 +1100

    assert admin permission

--- XF/Admin/Controller/Tools.php
+++ XF/Admin/Controller/Tools.php
@@ -5,6 +5,7 @@ class Tools extends XFCP_Tools
 	public function actionTestSparkPostMail()
 	{
 		$this->setSectionContext('sparkpostmailTest');
+        $this->assertAdminPermission('option');
 
 		$messages = [];
 		$results = false;
